ae17ab479f16585cfd2e089e73c79f642b7f5643
jhonshua <julio.llinas@gmail.com>
2023-08-13 22:55:12 -0400

    first commit

---
+++ src/components/about.jsx
@@ -0,0 +1,184 @@
+import React from 'react'
+import html from '../img/skill-icon/3basicas.png'
+import react from '../img/skill-icon/react.png'
+import php from '../img/skill-icon/php1.png'
+import mongose from '../img/skill-icon/mongo.png'
+import sql from '../img/skill-icon/mysql.png'
+import bootstrap from '../img/skill-icon/bootstrap.png'
+import Expo from '../img/skill-icon/expo 2.png'
+import Laravel from '../img/skill-icon/laravel.png'
+import pdf from '../img/skill-icon/pdf-download.png'
+
+class About extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      skills: [
+        {
+          id: 'HTML5_skill',
+          content: 'HTML5',
+          porcentage: '80%',
+          value: '90',
+          imagen: html,
+        },
+        {
+          id: 'ReactJS_skill',
+          content: 'ReactJS',
+          porcentage: '80%',
+          value: '80',
+          imagen: react,
+        },
+        {
+          id: 'Expo',
+          content: 'Expo',
+          porcentage: '85%',
+          value: '85',
+          imagen: Expo,
+        },
+        {
+          id: 'Bootstrap',
+          content: 'Bootstrap',
+          porcentage: '80%',
+          value: '80',
+          imagen: bootstrap,
+        },
+        {
+          id: 'PHP_skill',
+          content: 'PHP',
+          porcentage: '70%',
+          value: '70',
+          imagen: php,
+        },
+        {
+          id: 'Laravel',
+          content: 'Laravel',
+          porcentage: '80%',
+          value: '80',
+          imagen: Laravel,
+        },
+        {
+          id: 'Mongoose',
+          content: 'Mongoose',
+          porcentage: '75%',
+          value: '75',
+          imagen: mongose,
+        },
+        {
+          id: 'SQL',
+          content: 'SQL',
+          porcentage: '75%',
+          value: '75',
+          imagen: sql,
+        },
+      ],
+      about_me: [
+        {
+          id: 'first-p-about',
+          content:
+            'I am an electronic engineer with 1 year of experience as a full-stack developer. I am proficient in React JS, Expo, Express JS, PHP, Laravel, Photoshop, Figma, Unity, and Phaser. I am also an enthusiast of water sports, music, motorcycles, and nature.',
+        },
+        {
+          id: 'second-p-about',
+          content:
+            'As a full-stack developer, I have experience in front-end and back-end development. I am able to build websites and mobile apps from scratch, as well as integrate them with databases and third-party services. I am also an expert in using different programming languages and frameworks, including React JS, Expo, Express JS, PHP, Laravel, Photoshop, Figma, Unity, and Phaser. I am a disciplined and creative programmer with a strong desire to learn and grow. I am always looking for new ways to improve my skills and knowledge. I am also very motivated and I am always willing to work hard to achieve my goals.',
+        },
+        {
+          id: 'third-p-about',
+          content:
+            'I am looking for a position in a company where I can use my skills and experience to create innovative products and services. I am confident that I can be a valuable asset to your team and I am eager to contribute to your success.',
+        },
+      ],
+    }
+  }
+
+  render() {
+    return (
+      <section id='about' className='about-mf sect-pt4 route '>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-sm-12'>
+              <div className='box-shadow-full'>
+                <div className='row'>
+                  <div className='col-md-6'>
+                    <div className='row'>
+                      <div
+                        className='col-sm-6 col-md-5'
+                        style={{ margin: '0 auto' }}>
+                        <div
+                          className='about-img'
+                          style={{ textAlign: 'center' }}>
+                          <img
+                            className='img-fluid rounded b-shadow-a'
+                            alt=''
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className='skill-mf'>
+                      {/* <p className="title-s">Skill</p> */}
+                      {this.state.skills.map(skill => {
+                        return (
+                          <React.Fragment key={skill.id}>
+                            <span>
+                              {skill.content}
+                              <img
+                                style={{ maxWidth: '90px', margin: '10px' }}
+                                src={skill.imagen}
+                                alt='no found'
+                              />
+                            </span>{' '}
+                            <span className='pull-right'>
+                              {skill.porcentage}
+                            </span>
+                            <div className='progress'>
+                              <div
+                                className='progress-bar'
+                                role='progressbar'
+                                style={{ width: skill.porcentage }}
+                                aria-valuenow={skill.value}
+                                aria-valuemin='0'
+                                aria-valuemax='100'></div>
+                            </div>
+                          </React.Fragment>
+                        )
+                      })}
+                    </div>
+                  </div>
+                  <div className='col-md-6'>
+                    <div className='about-me pt-4 pt-md-0'>
+                      <div className='title-box-2'>
+                        <h5 className='title-left'>About Me</h5>
+                      </div>
+                      {this.state.about_me.map(content => {
+                        return (
+                          <p className='lead' key={content.id}>
+                            {content.content}
+                          </p>
+                        )
+                      })}
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <h6>download my resume </h6>
+                  <a
+                    href='https://drive.google.com/uc?export=download&id=1EFKxV-diV8KCi_o1EvcS73PFwQsIjZBi'
+                    download='resume.pdf'>
+                    {' '}
+                    <img
+                      src={pdf}
+                      style={{ maxWidth: '50px', margin: '10px' }}
+                      alt='no found'
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default About
---
+++ src/components/portfolio.jsx
@@ -0,0 +1,385 @@
+import React from 'react'
+
+//import stock
+
+import sitamm1 from '../img/sitamm1.png'
+import sitamm2 from '../img/sitamm2.png'
+import sitamm3 from '../img/sitamm3.png'
+import sitamm4 from '../img/sitamm4.png'
+
+import pampas1 from '../img/pampas1.png'
+import pampas2 from '../img/pampas2.png'
+import pampas3 from '../img/pampas3.png'
+import pampas4 from '../img/pampas4.png'
+
+import cash3 from '../img/cash3.jpg'
+import cash1 from '../img/cash1.jpg'
+import cash2 from '../img/cash2.jpg'
+
+import Hrsistem1 from '../img/Hrsistem1.png'
+import Hrsistem2 from '../img/Hrsistem2.jpeg'
+import Hrsistem3 from '../img/Hrsistem3.jpeg'
+
+import gym1 from '../img/gym1.png'
+import gym2 from '../img/gym2.png'
+import gym3 from '../img/gym3.png'
+import gym4 from '../img/gym4.png'
+
+import stock from '../img/image1.jpg'
+import stock1 from '../img/image2.jpeg'
+import stock2 from '../img/image3.png'
+import stock3 from '../img/image4.jpg'
+import stock4 from '../img/image5.png'
+
+class Portfolio extends React.Component {
+  render() {
+    return (
+      <section id='work' className='portfolio-mf sect-pt4 route'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-sm-12'>
+              <div className='title-box text-center'>
+                <h3 className='title-a'>Portfolio</h3>
+                <p className='subtitle-a'>
+                  some of my projects in which I have participated.
+                </p>
+                <div className='line-mf'></div>
+              </div>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col-md-4'>
+              <div className='work-box'>
+                <a href={sitamm1} data-lightbox='gallery-sitamm'>
+                  <div className='work-img'>
+                    <img src={sitamm1} alt='' className='img-fluid' />
+                  </div>
+                  <div className='work-content'>
+                    <div className='row'>
+                      <div className='col-sm-8'>
+                        <h2 className='w-title'>MERN project</h2>
+                        <div className='w-more'>
+                          <span className='w-ctegory'>
+                            Development project of a MERN application with which
+                            employees could have different benefits such as
+                            credits.
+                          </span>
+                        </div>
+                      </div>
+                      <div className='col-sm-4'>
+                        <div className='w-like'>
+                          <span className='ion-ios-plus-outline'></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+
+                <a
+                  href={sitamm2}
+                  data-lightbox='gallery-sitamm'
+                  style={{ display: 'none' }}>
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={sitamm3}
+                  data-lightbox='gallery-sitamm'
+                  style={{ display: 'none' }}>
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={sitamm4}
+                  data-lightbox='gallery-sitamm'
+                  style={{ display: 'none' }}>
+                  jsx-a11y/anchor-has-content warning
+                </a>
+
+                <a href='https://admin.sitamm.com/login' target='_blank'>
+                  <h6 className='col-sm-8'>production project</h6>
+                </a>
+
+                <a href='https://github.com/jhonshua/sitamm' target='_blank'>
+                  <h6 className='col-sm-8'>a bit of the code</h6>
+                </a>
+                <br />
+              </div>
+            </div>
+
+            <div className='col-md-4'>
+              <div className='work-box'>
+                <a href={pampas1} data-lightbox='gallery-pampas'>
+                  <div className='work-img'>
+                    <img src={pampas1} alt='' className='img-fluid' />
+                  </div>
+                  <div className='work-content'>
+                    <div className='row'>
+                      <div className='col-sm-8'>
+                        <h2 className='w-title'>MERN project</h2>
+                        <div className='w-more'>
+                          <span className='w-ctegory'>
+                            Development project of a MERN application with which
+                            employees can keep track of the differences in
+                            incidents in the company's facilities and design of
+                            the web page
+                          </span>{' '}
+                        </div>
+                      </div>
+                      <div className='col-sm-4'>
+                        <div className='w-like'>
+                          <span className='ion-ios-plus-outline'></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={pampas2}
+                  data-lightbox='gallery-pampas'
+                  style={{ display: 'none' }}>
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={pampas3}
+                  data-lightbox='gallery-pampas'
+                  style={{ display: 'none' }}>
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={pampas4}
+                  data-lightbox='gallery-pampas'
+                  style={{ display: 'none' }}>
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a href='http://128.199.6.217:81/auth/login' target='_blank'>
+                  <h6 className='col-sm-8'>production project MERN</h6>
+                </a>
+                <a href='https://grupopampas.com.mx/' target='_blank'>
+                  <h6 className='col-sm-8'>production WEB</h6>
+                </a>
+                <br />
+              </div>
+            </div>
+
+            <div className='col-md-4'>
+              <div className='work-box'>
+                <a href={cash1} data-lightbox='gallery-cash'>
+                  <div className='work-img'>
+                    <img src={cash1} alt='' className='img-fluid' />
+                  </div>
+                  <div className='work-content'>
+                    <div className='row'>
+                      <div className='col-sm-8'>
+                        <h2 className='w-title'>
+                          React Js Project Demo presentation
+                        </h2>
+                        <div className='w-more'>
+                          <span className='w-ctegory'>
+                            React Js,Bootstrap,redux toolkit.
+                          </span>
+                          {/*} / <span className="w-date">18 Sep. 2018</span>*/}
+                        </div>
+                      </div>
+                      <div className='col-sm-4'>
+                        <div className='w-like'>
+                          <span className='ion-ios-plus-outline'></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={cash2}
+                  data-lightbox='gallery-cash'
+                  style={{ display: 'none' }}>
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={cash3}
+                  data-lightbox='gallery-cash'
+                  style={{ display: 'none' }}>
+                  jsx-a11y/anchor-has-content warning
+                </a>
+              </div>
+            </div>
+
+            <div className='col-md-4'>
+              <div className='work-box'>
+                <a href={Hrsistem1} data-lightbox='gallery-medlingos'>
+                  <div className='work-img'>
+                    <img src={Hrsistem1} alt='' className='img-fluid' />
+                  </div>
+                  <div className='work-content'>
+                    <div className='row'>
+                      <div className='col-sm-8'>
+                        <h2 className='w-title'>
+                          Payroll and employee control project Laravel-PHP
+                        </h2>
+                        <div className='w-more'>
+                          <span className='w-ctegory'>PHP Laravel</span>
+                        </div>
+                      </div>
+                      <div className='col-sm-4'>
+                        <div className='w-like'>
+                          <span className='ion-ios-plus-outline'></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={Hrsistem2}
+                  data-lightbox='gallery-medlingos'
+                  style={{ display: 'none' }}>
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={Hrsistem3}
+                  data-lightbox='gallery-medlingos'
+                  style={{ display: 'none' }}>
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href='http://v3.desarrollo-hrsystem.site/login'
+                  target='_blank'>
+                  <h6 className='col-sm-8'>production project MERN</h6>
+                </a>
+                <a href='https://github.com/jhonshua/Poryecto-Hr-PHP'>
+                  <h6 className='col-sm-8' target='_blank'>
+                    a bit of the code
+                  </h6>
+                </a>
+                <br />
+              </div>
+            </div>
+
+            <div className='col-md-4'>
+              <div className='work-box'>
+                <a href={gym1} data-lightbox='gallery-smth'>
+                  <div className='work-img'>
+                    <img src={gym1} alt='' className='img-fluid' />
+                  </div>
+                  <div className='work-content'>
+                    <div className='row'>
+                      <div className='col-sm-8'>
+                        <h2 className='w-title'> Web page</h2>
+                        <div className='w-more'>
+                          <span className='w-ctegory'>HTML and Js project</span>
+                        </div>
+                      </div>
+                      <div className='col-sm-4'>
+                        <div className='w-like'>
+                          <span className='ion-ios-plus-outline'></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={gym2}
+                  data-lightbox='gallery-smth'
+                  style={{ display: 'none' }}>
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={gym3}
+                  data-lightbox='gallery-smth'
+                  style={{ display: 'none' }}>
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={gym3}
+                  data-lightbox='gallery-smth'
+                  style={{ display: 'none' }}>
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={gym4}
+                  data-lightbox='gallery-smth'
+                  style={{ display: 'none' }}>
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href='http://v3.desarrollo-hrsystem.site/login'
+                  target='_blank'>
+                  <h6 className='col-sm-8'>production project MERN</h6>
+                </a>
+                <br />
+              </div>
+            </div>
+            <div className='col-md-4'>
+              <div className='work-box'>
+                <a href={stock} data-lightbox='gallery-mf'>
+                  <div className='work-img'>
+                    <img src={stock} alt='' className='img-fluid' />
+                  </div>
+                  <div className='work-content'>
+                    <div className='row'>
+                      <div className='col-sm-8'>
+                        <h2 className='w-title'>Electronic Engineering</h2>
+                        <div className='w-more'>
+                          <span className='w-ctegory'>
+                            arduino project C++ and assembler languages
+                          </span>
+                        </div>
+                      </div>
+                      <div className='col-sm-4'>
+                        <div className='w-like'>
+                          <span className='ion-ios-plus-outline'></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={stock1}
+                  data-lightbox='gallery-mf'
+                  style={{ display: 'none' }}>
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={stock2}
+                  data-lightbox='gallery-mf'
+                  style={{ display: 'none' }}>
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={stock3}
+                  data-lightbox='gallery-mf'
+                  style={{ display: 'none' }}>
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={stock4}
+                  data-lightbox='gallery-mf'
+                  style={{ display: 'none' }}>
+                  jsx-a11y/anchor-has-content warning
+                </a>
+
+                <a
+                  href='https://www.tinkercad.com/things/fBD0Yd6A1xS?sharecode=Kl2YphV48lpOO3Ggt78QvGFdL44NoTtkxX6JTWJWxfc'
+                  target='_blank'>
+                  <h6 className='col-sm-8'>
+                    - 2x16 lcd message with elapsed seconds count C++
+                  </h6>
+                </a>
+                <a
+                  href='https://www.tinkercad.com/things/3TI6225UrrN?sharecode=Wt_SpFRT2PfO_DU4c1TpiM67ave82jirp-pcjGySPaE'
+                  target='_blank'>
+                  <h6 className='col-sm-8'>- sonar distance meter C++</h6>
+                </a>
+                <a
+                  href='https://www.tinkercad.com/things/kiyuj1rw0Qm?sharecode=B9kUZDTB_95BCO1di-kvsjiDFNFgdJaBg6NMWFtJtV8'
+                  target='_blank'>
+                  <h6 className='col-sm-8'>- c++ game</h6>
+                </a>
+                <br />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Portfolio
